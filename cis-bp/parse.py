24bb9271d12bfc99d9de197615aba980081c4051
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-10-10 10:06:40 -0700

    encapsulated code in functions

--- cis-bp/parse.py
+++ cis-bp/parse.py
@@ -108,8 +108,8 @@ def parse_cisbp(cisbp_dir, output_dir="./"):
     # Get k-mers
     _get_kmers(cisbp_dir, output_dir)
 
-    # Cis-BP to MEME format
-    _reformat_to_meme(cisbp_dir, output_dir)
+    # # Cis-BP to MEME format
+    # _reformat_to_meme(cisbp_dir, output_dir)
 
     # Group TFs by DBD composition
     _group_by_DBD_composition(cisbp_dir, output_dir)
