db20c6d2d44ee250c790eb5ac25e437708e6bc64
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-10-15 10:23:13 -0700

    fixed bug ZeroDivisionError: float division by zero

--- cis-bp/parse.py
+++ cis-bp/parse.py
@@ -366,10 +366,12 @@ def _group_by_kmer_overlap(output_dir=out_dir):
             # For each matrix ID...
             for next_matrix, evalue in tomtom[matrix]:
 
-                intersection = len(kmers[matrix].intersection(kmers[next_matrix]))
-                union = len(kmers[matrix].union(kmers[next_matrix]))
-                print(intersection, union)
-                overlaps[matrix].append([next_matrix, float(intersection) / union])
+                if len(kmers[matrix]) == 0:
+                    overlaps[matrix].append([next_matrix, 0.0])
+                else:
+                    intersection = len(kmers[matrix].intersection(kmers[next_matrix]))
+                    union = len(kmers[matrix].union(kmers[next_matrix]))
+                    overlaps[matrix].append([next_matrix, float(intersection) / union])
 
         # Write
         Jglobals.write(
