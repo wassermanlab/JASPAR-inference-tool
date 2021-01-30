4a26b680140dceb31e6714faa332dc668c428446
Oriol Fornes <oriol.fornes@gmail.com>
2021-01-29 16:23:39 -0800

    added arabidopsis and maize predictions

---
+++ examples/plants/uniprot.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Arabidopsis
+wget -q ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000006548_3702.fasta.gz
+# Maize
+wget -q ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000007305_4577.fasta.gz
